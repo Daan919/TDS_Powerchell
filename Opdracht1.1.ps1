5604977e7c50bd2ea65ad8767ddeacb8205e295b
Luuk <luukvanuden2199@gmail.com>
2020-03-02 13:09:44 +0100

    add comments

--- Opdracht1.1.ps1
+++ Opdracht1.1.ps1
@@ -1,7 +1,8 @@
 
-# genarate new GUID for the vm   
+# Genarate new GUID for the VM   
 $JobGroup_IO = [guid]::NewGuid().ToString()
 $JobGroup_VM = [guid]::NewGuid().ToString()
+# VM variables 
 $HW_profile = "W2019-HP1-VLAN0"
 $VM_Template = "W2019_Template_02"
 $temp_template = "Temporary Template" +[guid]::NewGuid().ToString()
@@ -9,11 +10,12 @@ $PcName = "computernaam_4"
 $VM_name = "vm04"
 $VM_disctiption = "VM - nummer - 04"
 
+# Create administrator credentials
 $password = ConvertTo-SecureString “Password01” -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ("administrator", $password)
 
 
-# create en set IO VM
+# Create en set IO VM
 New-SCVirtualScsiAdapter -VMMServer localhost -JobGroup $JobGroup_IO -AdapterID 7 -ShareVirtualScsiAdapter $false -ScsiControllerType DefaultTypeNoType 
 $ISO = Get-SCISO -VMMServer localhost -ID "20067900-bd4c-4e57-bda4-7ea35dee51c1" | where {$_.Name -eq "en_windows_server_2019_x64_dvd_4cb967d8.iso"}
 New-SCVirtualDVDDrive -VMMServer localhost -JobGroup $JobGroup_IO -Bus 1 -LUN 0 -ISO $ISO 
@@ -24,20 +26,22 @@ Set-SCVirtualCOMPort -NoAttach -VMMServer localhost -GuestPort 1 -JobGroup $JobG
 Set-SCVirtualCOMPort -NoAttach -VMMServer localhost -GuestPort 2 -JobGroup $JobGroup_IO 
 Set-SCVirtualFloppyDrive -RunAsynchronously -VMMServer localhost -NoMedia -JobGroup $JobGroup_IO 
 
-
+# Create temporary template with selected template and hardware profile
 $Template = Get-SCVMTemplate -VMMServer localhost -ID "852dc4b4-a6c2-4f44-bf81-9e48918fc5ae" | where {$_.Name -eq $VM_template }
 $HardwareProfile = Get-SCHardwareProfile -VMMServer localhost | where {$_.Name -eq $HW_profile}
 $OperatingSystem = Get-SCOperatingSystem -VMMServer localhost -ID "dffb90ce-abb0-4082-8764-fb08db195c05" | where {$_.Name -eq "Windows Server 2019 Standard"}
 
 New-SCVMTemplate -Name $temp_template -Template $Template -HardwareProfile $HardwareProfile -JobGroup $JobGroup_VM -ComputerName "comp01" -TimeZone 110  -Workgroup "WORKGROUP" -AnswerFile $null -OperatingSystem $OperatingSystem -UpdateManagementProfile $null 
 
-
+# Assign temporary template to VM
 $template = Get-SCVMTemplate -All | where { $_.Name -eq $temp_template }
 $virtualMachineConfiguration = New-SCVMConfiguration -VMTemplate $template -Name $VM_name
 Write-Output $virtualMachineConfiguration
 
+# Get VMM host to store VM
 $vmHost = Get-SCVMHost -ID "50fcc871-63a8-4f03-903b-8e356172f64f"
 
+# Create VM with given variables
 Set-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration -VMHost $vmHost
 Update-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration
 Set-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration -ComputerName $PcName
