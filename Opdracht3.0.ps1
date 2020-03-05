8f7b6cd345de94b504af4dc3635d045bd2b38d05
Daan <daan9191@gmail.com>
2020-03-05 13:17:58 +0100

    finish

--- Opdracht3.0.ps1
+++ Opdracht3.0.ps1
@@ -1,39 +1,23 @@
-#Import-CSV -Path .\Bulkuitrol.csv | Write-Host "VMnaam=" + $_.VMnaam
-
-#Import-CSV -Path .\Bulkuitrol.csv | ForEach-Object {Write-Host "VMnaam=" + $_.VMnaam}
-
-#$temp=gc "temp.csv"
-# $array=@()
-
-# $temp | Foreach{
-#     $elements=$_.split(",")
-#     $array+= ,@($elements[0],$elements[1],$elements[2])
-# }
-
-# foreach($value in $array)
-# {
-#     write-host "EmpID =" $value[0] "and Name =" $value[1] "and Dept =" $value[2]
-# }
-
 
 $virtualmachines = Import-Csv -Path .\Bulkuitrol.csv
 foreach($VM in $virtualmachines){
-    
-    # Genarate new GUID for the VM   
+    # Genarate new GUID for the VM and temp template job's  
     $JobGroup_IO = [guid]::NewGuid().ToString()
     $JobGroup_VM = [guid]::NewGuid().ToString()
-    # VM variables 
-    $HW_profile = "W2019-HP1-VLAN0"
-    $VM_Template = "W2019_Template_02"
     $temp_template = "Temporary Template" +[guid]::NewGuid().ToString()
+    
+    # VM variables 
+    
     $PcName = $VM.CompNaam
     $VM_name = $VM.VMnaam
     $VM_disctiption = $VM.VMdesc
+    $Password = $VM.Password
+    $VM_Template = $VM.Template
+    $HW_profile = $VM.HardwareProfile
 
     # Create administrator credentials
     $password = ConvertTo-SecureString “Password01” -AsPlainText -Force
     $Cred = New-Object System.Management.Automation.PSCredential ("administrator", $password)
-
 
     # Create en set IO VM
     New-SCVirtualScsiAdapter -VMMServer localhost -JobGroup $JobGroup_IO -AdapterID 7 -ShareVirtualScsiAdapter $false -ScsiControllerType DefaultTypeNoType 
@@ -47,7 +31,7 @@ foreach($VM in $virtualmachines){
     Set-SCVirtualFloppyDrive -RunAsynchronously -VMMServer localhost -NoMedia -JobGroup $JobGroup_IO 
 
     # Create temporary template with selected template and hardware profile
-    $Template = Get-SCVMTemplate -VMMServer localhost -ID "852dc4b4-a6c2-4f44-bf81-9e48918fc5ae" | where {$_.Name -eq $VM_template }
+    $Template = Get-SCVMTemplate | where {$_.Name -eq $VM_template }
     $HardwareProfile = Get-SCHardwareProfile -VMMServer localhost | where {$_.Name -eq $HW_profile}
     $OperatingSystem = Get-SCOperatingSystem -VMMServer localhost -ID "dffb90ce-abb0-4082-8764-fb08db195c05" | where {$_.Name -eq "Windows Server 2019 Standard"}
 
@@ -65,7 +49,6 @@ foreach($VM in $virtualmachines){
     Set-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration -VMHost $vmHost
     Update-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration
     Set-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration -ComputerName $PcName
-    $AllNICConfigurations = Get-SCVirtualNetworkAdapterConfiguration -VMConfiguration $virtualMachineConfiguration
     Update-SCVMConfiguration -VMConfiguration $virtualMachineConfiguration
     New-SCVirtualMachine -Name $VM_name -VMConfiguration $virtualMachineConfiguration -Description $VM_disctiption -BlockDynamicOptimization $false -JobGroup $JobGroup_VM -ReturnImmediately -StartAction "NeverAutoTurnOnVM" -StopAction "SaveVM" -LocalAdministratorCredential $Cred
 }
